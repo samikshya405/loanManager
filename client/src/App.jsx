e23563666cd0272fb7db049d76b70420b68177d3
samikshya <samikshyakharel405@gmail.com>
2024-06-08 23:15:50 +1000

    created layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import Login from './pages/auth/Login'
+import { Route, Routes } from 'react-router-dom'
+import SignUp from './pages/auth/SignUp'
+import Dashboard from './pages/dashboard/Dashboard'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+
+      
+      <Routes>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/signup' element={<SignUp/>}/>
+        <Route path='/' element={<Dashboard/>}/>
+      </Routes>
+    </div>
   )
 }
 

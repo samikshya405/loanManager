d21825add8cc23e24325b76ed665222e6435cae8
samikshya <samikshyakharel405@gmail.com>
2024-06-09 23:34:24 +1000

    dashboard initialized

--- client/src/component/MainLayout.jsx
+++ client/src/component/MainLayout.jsx
@@ -1,17 +1,18 @@
-import { Box } from '@mui/material'
-import React from 'react'
+import { Box } from "@mui/material";
+import React from "react";
 
-const MainLayout = ({children}) => {
+const MainLayout = ({ children }) => {
   return (
-    <Box height={"100vh"} bgcolor={"var(--secondary)"} >
-        <Box  >
+    <Box
+      height={"100vh"}
+      bgcolor={"var(--secondary)"}
+      sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}
+    >
+      <Box minHeight={"80vh"} bgcolor={"white"} width={"80%"}>
         {children}
-        </Box>
-
-
-
+      </Box>
     </Box>
-  )
-}
+  );
+};
 
-export default MainLayout
+export default MainLayout;
--- client/src/pages/dashboard/Dashboard.jsx
+++ client/src/pages/dashboard/Dashboard.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import MainLayout from '../../component/MainLayout'
+import { Box } from '@mui/material'
 
 const Dashboard = () => {
   return (
     <MainLayout>
+      <Box>
+        dkfnj
+      </Box>
+
+      
         
     </MainLayout>
   )

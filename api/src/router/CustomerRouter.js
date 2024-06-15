d2ce49744ad716a75fe218248aeea43c4ba8f988
samikshya <samikshyakharel405@gmail.com>
2024-06-15 17:53:12 +1000

    logic for adding customer

---
+++ api/src/router/CustomerRouter.js
@@ -0,0 +1,36 @@
+import express from "express";
+import { findUser, postNewCustomer } from "../model/customer/CustomerModel.js";
+
+const router = express.Router();
+
+router.post("/", async (req, res) => {
+  const { authorization } = req.headers;
+  if (!authorization) {
+    return res.status(400).json({
+      status: "error",
+      message: "Authorization header is missing",
+    });
+  }
+  try {
+    // const {fName}= req.body
+    // const user =await findUser(fName)
+    // console.log(user);
+    const result = await postNewCustomer({
+      ...req.body,
+      userId: authorization,
+    });
+    result._id
+      ? res.json({
+          status: "success",
+          message: "New customer added",
+        })
+      : res.json({
+          status: "error",
+          message: "failed to add new user try again",
+        });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+export default router;

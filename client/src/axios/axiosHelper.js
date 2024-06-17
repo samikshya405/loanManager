0963ca53d297ff7f930c0682e80fadf3aa540e19
samikshya <samikshyakharel405@gmail.com>
2024-06-17 19:03:47 +1000

    axios added to connect with api

--- client/src/axios/axiosHelper.js
+++ client/src/axios/axiosHelper.js
@@ -1,50 +1,67 @@
 import axios from "axios";
+import { useSelector } from "react-redux";
 const rootAPI = import.meta.env.VITE_APP_ROOTAPI;
 const userEP = rootAPI + "/users";
+const customerEp = rootAPI + "/customers";
 
 export const postNewUser = async (userObj) => {
-    try {
-      const { data } = await axios.post(userEP, userObj);
-      return data;
-    } catch (error) {
-      console.log(error);
-      return {
-        status: "error",
-        message: error.message,
-      };
-    }
-  };
-  export const userLogin = async (loginInfo) => {
-    try {
-      const { data } = await axios.post(userEP + "/login", loginInfo);
-      return data;
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  try {
+    const { data } = await axios.post(userEP, userObj);
+    return data;
+  } catch (error) {
+    console.log(error);
+    return {
+      status: "error",
+      message: error.message,
+    };
+  }
+};
+export const userLogin = async (loginInfo) => {
+  try {
+    const { data } = await axios.post(userEP + "/login", loginInfo);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-  export const fetchUserInfo = async() => {
-   
-  
-    try {
-      const headers = {
+export const fetchUserInfo = async () => {
+  try {
+    const headers = {
+      Authorization: getAccessJWT(),
+    };
+    const data = await axios.get(userEP, {
+      headers: {
         Authorization: getAccessJWT(),
-      };
-      const data = await axios.get(userEP, {
-        headers: {
-          Authorization: getAccessJWT(),
-        },
-      });
-      return data.data.user
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  
-  const getAccessJWT = () => {
-    return sessionStorage.getItem("accessJWT");
-  };
-  export const logout = () => {
-    sessionStorage.removeItem('accessJWT');
-    localStorage.removeItem('refreshJWT');
-  };
+      },
+    });
+    return data.data.user;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getAccessJWT = () => {
+  return sessionStorage.getItem("accessJWT");
+};
+
+export const postNewCustomer = async (userId, customerInfo) => {
+  try {
+    const response = await axios.post(customerEp, customerInfo, {
+      headers: {
+        Authorization: userId,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    return {
+      status: "error",
+      message: error.message,
+    };
+  }
+};
+export const logout = () => {
+  sessionStorage.removeItem("accessJWT");
+  localStorage.removeItem("refreshJWT");
+};

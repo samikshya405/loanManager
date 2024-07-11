49ee2ba6729f675985d68dd23f8a490c8bad6693
samikshya <samikshyakharel405@gmail.com>
2024-07-11 20:35:12 +1000

    transaction router added

---
+++ api/src/router/transactionRouter.js
@@ -0,0 +1,16 @@
+import express from 'express'
+
+const router = express.Router()
+
+router.post('/',(req,res)=>{
+    try {
+        
+    } catch (error) {
+        console.log(error);
+        
+    }
+})
+
+
+
+export default router

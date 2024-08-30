71283e40de9494fd678ecf0356dfa76fc48be609
samikshya <samikshyakharel405@gmail.com>
2024-08-30 20:32:05 +1000

    main edited

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,17 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import {BrowserRouter} from 'react-router-dom'
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { Provider } from "react-redux";
+import store, { persistor } from "./redux/store.js";
+import { PersistGate } from "redux-persist/integration/react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-    <App />
-    <ToastContainer/>
+      <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+      </Provider>
+
+      <ToastContainer />
     </BrowserRouter>
-    
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
